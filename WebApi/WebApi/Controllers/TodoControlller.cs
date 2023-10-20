a02d3751001e1f378958503b00533b3f5069d877
hugo <hugo-mehdi.taj@epitech.eu>
2023-10-20 19:36:00 +0200

    [add] post delete modify and addpost finished

--- WebApi/WebApi/Controllers/TodoControlller.cs
+++ WebApi/WebApi/Controllers/TodoControlller.cs
@@ -1,50 +1,65 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using WebApi.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Collections.Generic;
+    using WebApi.Models;
 
-namespace WebApi.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class TodoController : ControllerBase {
-        
-        private readonly ILogger<TodoController> _logger;
+    namespace WebApi.Controllers
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class TodoController : ControllerBase {
+            
+            private readonly ILogger<TodoController> _logger;
 
-        public TodoController(ILogger<TodoController> logger)
-        {
-            _logger = logger;
-        }
-        
-        [HttpGet("list")]
-        public ActionResult<IEnumerable<Post>> Get() {
-            return Ok(ListPosts.listPosts);
-        }
-        
-        [HttpPost("add/{content}")]
-        public ActionResult<Post> Get(string content ) {
-            Post Post = new Post(content);
-            ListPosts.listPosts.Add(Post);
-            return Ok($"Post:  {Post.Task} id: {Post.TaskId} has been created");
-        }
-        
-        [HttpDelete("delete/{taskId}")]
-        public ActionResult<string> DeleteTask(int taskId)
-        {
-            // Recherchez la tâche dans la liste par son ID
-            Post taskToRemove = ListPosts.listPosts.Find(task => task.TaskId == taskId);
-
-            if (taskToRemove == null)
+            public TodoController(ILogger<TodoController> logger)
             {
-                return NotFound($"Task with ID {taskId} not found.");
+                _logger = logger;
             }
+            
+            [HttpGet("list")]
+            public ActionResult<IEnumerable<Post>> Get() {
+                return Ok(ListPosts.listPosts);
+            }
+            
+            [HttpPost("add/{content}")]
+            public ActionResult<Post> Get(string content ) {
+                Post Post = new Post(content);
+                ListPosts.listPosts.Add(Post);
+                return Ok($"Post:  {Post.Task} id: {Post.TaskId} has been created");
+            }
+            
+             [HttpPut("update/{taskId}")]
+            public ActionResult<string> UpdateTask(int taskId, [FromBody] string newContent)
+            {
+                // Recherchez la tâche dans la liste par son ID
+                Post taskToUpdate = ListPosts.listPosts.Find(task => task.Id == taskId);
 
-            // Supprimez la tâche de la liste
-            ListPosts.listPosts.Remove(taskToRemove);
+                if (taskToUpdate == null)
+                {
+                    return NotFound($"Task with ID {taskId} not found.");
+                }
 
-            return Ok($"Task with ID {taskId} has been deleted.");
+                // Mettez à jour le contenu de la tâche
+                taskToUpdate.Task = newContent;
+
+                return Ok($"Task with ID {taskId} has been updated.");
+            }
+            
+            [HttpDelete("delete/{taskId}")]
+            public ActionResult<string> DeleteTask(int taskId)
+            {
+                // Recherchez la tâche dans la liste par son ID
+                Post taskToRemove = ListPosts.listPosts.Find(task => task.TaskId == taskId);
+
+                if (taskToRemove == null)
+                {
+                    return NotFound($"Task with ID {taskId} not found.");
+                }
+
+                // Supprimez la tâche de la liste
+                ListPosts.listPosts.Remove(taskToRemove);
+
+                return Ok($"Task with ID {taskId} has been deleted.");
+            }
         }
-
-
     }
-}
--- WebApi/WebApi/WeatherForecast.cs
+++ WebApi/WebApi/WeatherForecast.cs
@@ -1,21 +1,57 @@
+using System;
+using System.Collections.Generic;
+
 namespace WebApi.Models
 {
     public static class ListPosts
     {
         public static List<Post> listPosts = new();
+
+        public static void AddPost(Post post)
+        {
+            listPosts.Add(post);
+        }
+
+        public static Post GetPostById(int taskId)
+        {
+            return listPosts.Find(task => task.TaskId == taskId);
+        }
+
+        public static string GetPostDescription(int taskId)
+        {
+            Post post = GetPostById(taskId);
+            return post != null ? post.Task : "Post not found";
+        }
+
+        public static void UpdatePost(int taskId, Post updatedPost)
+        {
+            Post postToUpdate = GetPostById(taskId);
+            if (postToUpdate != null)
+            {
+                postToUpdate.Task = updatedPost.Task;
+            }
+        }
+
+        public static void DeletePost(int taskId)
+        {
+            Post postToRemove = GetPostById(taskId);
+            if (postToRemove != null)
+            {
+                listPosts.Remove(postToRemove);
+            }
+        }
     }
 
     public class Post
     {
-        public int TaskId { get; set; } // Changé pour être une propriété d'instance
-
+        public int TaskId { get; set; }
         public int Id { get; set; }
         public string Task { get; set; }
 
         public Post(string post)
         {
             Task = post;
-            TaskId = ListPosts.listPosts.Count + 1; // Incrémenté en fonction du nombre d'éléments dans la liste
+            TaskId = ListPosts.listPosts.Count + 1;
             Id = TaskId;
         }
     }
