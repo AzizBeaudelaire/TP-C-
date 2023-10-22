1b113d28314caa9cf7f49b161bfd7d898188d4a2
hugo <hugo-mehdi.taj@epitech.eu>
2023-10-23 00:05:33 +0200

    [add] priorities, name func, ID

--- WebApi/WebApi/Controllers/TodoControlller.cs
+++ WebApi/WebApi/Controllers/TodoControlller.cs
@@ -1,114 +1,114 @@
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
-    using System.Collections.Generic;
-    using WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using WebApi.Models;
 
-    namespace WebApi.Controllers
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
     {
-        [ApiController]
-        [Route("[controller]")]
-        public class TodoController : ControllerBase {
-            
-            private readonly ILogger<TodoController> _logger;
+        private readonly ILogger<TodoController> _logger;
 
-            public TodoController(ILogger<TodoController> logger)
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("TaskBoard")]
+        public ActionResult<IEnumerable<Post>> Get()
+        {
+            // Tri des tâches par priorité en ordre croissant
+            var sortedTasks = ListPosts.listPosts.OrderBy(task => task.Priority).ToList();
+            return Ok(sortedTasks);
+        }
+
+        [HttpGet("Search/{id}")]
+        public ActionResult<Post> GetTodoById(int id)
+        {
+            Post todo = ListPosts.GetPostById(id);
+
+            if (todo == null)
             {
-                _logger = logger;
+                return NotFound($"Task with ID {id} not found.");
             }
-            
-            [HttpGet("list")]
-            public ActionResult<IEnumerable<Post>> Get() {
-                return Ok(ListPosts.listPosts);
-            }
-            
-            [HttpGet("get/{id}")]
-            public ActionResult<Post> GetTodoById(int id)
+
+            return Ok(todo);
+        }
+
+        [HttpPost("Add-a-task")]
+        public ActionResult<Post> AddTodo([FromBody] Post model)
+        {
+            // Vérifiez si le modèle est valide, y compris la priorité (par exemple, le contenu n'est pas vide et la priorité est valide).
+            if (string.IsNullOrEmpty(model.Task) || !Enum.IsDefined(typeof(Priority), model.Priority))
             {
-                Post todo = ListPosts.GetPostById(id);
-
-                if (todo == null)
-                {
-                    return NotFound($"Task with ID {id} not found.");
-                }
-
-                return Ok(todo);
+                return BadRequest("Task content cannot be empty, and priority must be valid.");
             }
-            
-            [HttpPost("add")]
-            public ActionResult<Post> AddTodo([FromBody] Post model)
+
+            // Générez un nouvel identifiant pour la tâche.
+            model.Id = ListPosts.listPosts.Count + 1; // Utilisation de la propriété Id
+
+            // Ajoutez la nouvelle tâche à la liste.
+            ListPosts.AddPost(model);
+
+            // Convention REST : retournez un code 201 Created avec un en-tête Location.
+            // L'en-tête Location spécifie l'URL pour récupérer cet élément nouvellement créé.
+            return CreatedAtAction(nameof(GetTodoById), new { id = model.Id }, model);
+        }
+
+        [HttpPut("Update-a-task/{taskId}")]
+        public ActionResult<string> UpdateTask(int taskId, [FromBody] string newContent)
+        {
+            // Recherchez la tâche dans la liste par son ID
+            Post taskToUpdate = ListPosts.listPosts.Find(task => task.Id == taskId);
+
+            if (taskToUpdate == null)
             {
-                // Vérifiez si le modèle est valide (par exemple, le contenu n'est pas vide).
-                if (string.IsNullOrEmpty(model.Task))
-                {
-                    return BadRequest("Task content cannot be empty.");
-                }
-
-                // Générez un nouvel identifiant pour la tâche.
-                model.TaskId = ListPosts.listPosts.Count + 1;
-                model.Id = model.TaskId;
-
-                // Ajoutez la nouvelle tâche à la liste.
-                ListPosts.AddPost(model);
-
-                // Convention REST : retournez un code 201 Created avec un en-tête Location.
-                // L'en-tête Location spécifie l'URL pour récupérer cet élément nouvellement créé.
-                return CreatedAtAction(nameof(GetTodoById), new { id = model.Id }, model);
+                return NotFound($"Task with ID {taskId} not found.");
             }
 
-            
-             [HttpPut("update/{taskId}")]
-            public ActionResult<string> UpdateTask(int taskId, [FromBody] string newContent)
+            // Mettez à jour le contenu de la tâche
+            taskToUpdate.Task = newContent;
+
+            return Ok($"Task with ID {taskId} has been updated.");
+        }
+
+        [HttpPatch("Set-as-done/{id}")]
+        public ActionResult SetTodoAsDone(long id)
+        {
+            Post post = ListPosts.listPosts.Find(t => t.Id == id);
+
+            if (post == null)
             {
-                // Recherchez la tâche dans la liste par son ID
-                Post taskToUpdate = ListPosts.listPosts.Find(task => task.Id == taskId);
-
-                if (taskToUpdate == null)
-                {
-                    return NotFound($"Task with ID {taskId} not found.");
-                }
-
-                // Mettez à jour le contenu de la tâche
-                taskToUpdate.Task = newContent;
-
-                return Ok($"Task with ID {taskId} has been updated.");
+                return NotFound($"Post with ID {id} not found.");
             }
-            
-            
-            [HttpPatch("set-done/{id}")]
-            public ActionResult SetTodoAsDone(long id)
+
+            if (post.IsDone)
             {
-                Post post = ListPosts.listPosts.Find(t => t.TaskId == id);
-
-                if (post == null)
-                {
-                    return NotFound($"Post with ID {id} not found.");
-                }
-
-                if (post.IsDone)
-                {
-                    return BadRequest("Post is already done.");
-                }
-
-                post.IsDone = true;
-
-                return Ok();    
+                return BadRequest("Post is already done.");
             }
-            
-            [HttpDelete("delete/{taskId}")]
-            public ActionResult<string> DeleteTask(int taskId)
+
+            post.IsDone = true;
+
+            return Ok();
+        }
+
+        [HttpDelete("Delete-a-task/{taskId}")]
+        public ActionResult<string> DeleteTask(int taskId)
+        {
+            // Recherchez la tâche dans la liste par son ID
+            Post taskToRemove = ListPosts.listPosts.Find(task => task.Id == taskId);
+
+            if (taskToRemove == null)
             {
-                // Recherchez la tâche dans la liste par son ID
-                Post taskToRemove = ListPosts.listPosts.Find(task => task.TaskId == taskId);
-
-                if (taskToRemove == null)
-                {
-                    return NotFound($"Task with ID {taskId} not found.");
-                }
-
-                // Supprimez la tâche de la liste
-                ListPosts.listPosts.Remove(taskToRemove);
-
-                return Ok($"Task with ID {taskId} has been deleted.");
+                return NotFound($"Task with ID {taskId} not found.");
             }
+
+            // Supprimez la tâche de la liste
+            ListPosts.listPosts.Remove(taskToRemove);
+
+            return Ok($"Task with ID {taskId} has been deleted.");
         }
     }
+}
